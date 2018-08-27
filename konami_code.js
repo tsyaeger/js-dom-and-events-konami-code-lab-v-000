5cb7c8c3c8a53b5683680a709016b507fdda6734
taryn yaeger <tsyaeger@gmail.com>
2018-08-27 06:37:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,7 +7,7 @@ function init() {
   const body = document.querySelector('body')
    
   body.addEventListener('keydown', function(e) {
-    alert(e.which)
+    let key = parseInt(e.detail || e.which);
   })
   
   
