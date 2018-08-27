8926f1a427bd8f00e925a7e3438a45bd00dd389d
taryn yaeger <tsyaeger@gmail.com>
2018-08-27 06:34:56 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,10 +3,11 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
 
-}
-
 const body = document.querySelector('body')
  
 body.addEventListener('keydown', function(e) {
   alert(e.which)
 })
+
+
+}
